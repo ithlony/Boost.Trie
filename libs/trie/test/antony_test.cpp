b263a6e36b7512950fa4dbfcd3a163a98ca0f9cb
hardy <hzithlony@git.com>
2013-08-04 23:00:18 +0800

    test of travis-ci

---
+++ libs/trie/test/antony_test.cpp
@@ -0,0 +1,125 @@
+#define BOOST_TEST_MODULE trie_test
+#include <boost/test/unit_test.hpp>
+#include "boost/trie/trie.hpp"
+#include "boost/trie/trie.hpp"
+#include <string>
+#include <iostream>
+#include <map>
+
+BOOST_AUTO_TEST_SUITE(trie_test__mentor_antony)
+
+BOOST_AUTO_TEST_CASE(default_construction_and_destruction)
+{
+    boost::tries::trie_map<std::string, std::string> t;
+    (void)t;
+}
+
+class no_default_constructor_type {
+    explicit no_default_constructor_type(int /*i*/){}
+};
+
+//@ Does not compile. However it MUST!
+BOOST_AUTO_TEST_CASE(no_default_constructor_key)
+{
+	std::map<no_default_constructor_type, int> t;
+	(void)t;
+	/*
+    boost::tries::trie_map<no_default_constructor_type, int> t;
+    (void)t;
+	*/
+}
+
+class counter_type {
+    static int m_counts_constructed;
+    static int m_counts_deleted;
+
+    int m_value;
+    int m_deleted;
+public:
+    static void reset() {
+        m_counts_constructed = 0;
+        m_counts_deleted = 0;
+    }
+
+    static int counts_constructed() { return m_counts_constructed; }
+    static int counts_deleted() { return m_counts_deleted; }
+    static int counts_alive() { return m_counts_constructed - m_counts_deleted; }
+
+    counter_type() : m_value(-1), m_deleted(0) { ++ m_counts_constructed; }
+    counter_type(int value) : m_value(value), m_deleted(0) { ++ m_counts_constructed; }
+    counter_type(const counter_type& ct) : m_value(ct.m_value), m_deleted(0) { ++ m_counts_constructed; }
+
+    ~counter_type() {
+        ++ m_deleted;
+        BOOST_CHECK_EQUAL(m_deleted, 1);
+        ++ m_counts_deleted;
+    }
+
+    int value() const { return m_value; }
+};
+
+
+int counter_type::m_counts_constructed = 0;
+int counter_type::m_counts_deleted = 0;
+
+inline bool operator< (const counter_type& v1, const counter_type& v2) {
+    return v1.value() < v2.value();
+}
+
+BOOST_AUTO_TEST_CASE(counting_constructions_and_destructions_key)
+{
+    counter_type::reset();
+    std::vector<counter_type> data;
+    data.push_back(counter_type(0));
+    data.push_back(counter_type(0));
+    data.push_back(counter_type(1));
+    data.push_back(counter_type(2));
+    data.push_back(counter_type(3));
+    const int data_size = static_cast<int>(data.size());
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size);
+
+    boost::tries::trie_map<counter_type, int> t;
+	std::cout << counter_type::counts_alive() << std::endl;
+    t.insert(data, 0);
+	std::cout << counter_type::counts_alive() << std::endl;
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+
+    t.insert(data, 0);
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+
+    t.insert(data.begin(), data.end(), 0);
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+
+    t.insert(data.begin() + 1, data.end(), 0);
+    //BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 3 - 1);
+	//the following statement is the correct one
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 3 - 2);
+
+    t.clear();
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size);
+}
+
+BOOST_AUTO_TEST_CASE(counting_alive_equal_keys)
+{
+    counter_type::reset();
+    // Inserting 25 elements with value 0
+    std::vector<counter_type> data(25,counter_type(0));
+    const int data_size = static_cast<int>(data.size());
+
+    boost::tries::trie_map<counter_type, int> t;
+    t.insert(data, 0);
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+
+    t.insert(data, 0);
+    BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+
+    std::vector<counter_type>::const_iterator it = data.begin(),
+            end = data.end();
+    for (; it != end; ++it)
+    {
+        t.insert(it, end, 0);
+        BOOST_CHECK_EQUAL(counter_type::counts_alive(), data_size * 2);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ libs/trie/test/test_custom_type.cpp
@@ -0,0 +1,264 @@
+#define BOOST_TEST_MODULE trie_test
+#include <boost/test/unit_test.hpp>
+#include "boost/trie/trie.hpp"
+#include "boost/trie/trie.hpp"
+#include <string>
+#include <iostream>
+
+BOOST_AUTO_TEST_SUITE(trie_test)
+
+class mytype {
+
+public:
+	explicit mytype() : value(0)
+	{}
+
+	mytype(int x) : value(x)
+	{
+	}
+
+	mytype& operator=(const mytype& x)
+	{
+		value = x.value;
+		return *this;
+	}
+
+	bool operator<(const mytype& x) const
+	{
+		return value < x.value;
+	}
+	bool operator==(const mytype& x) const
+	{
+		return value == x.value;
+	}
+
+	int value;
+};
+
+typedef boost::tries::trie_map<mytype, mytype> tmm;
+typedef std::vector<mytype> vm;
+
+std::ostream& operator<<(std::ostream& out, const mytype& m)
+{
+	out << m.value;
+	return out;
+}
+
+mytype ms1[] = {1, 2, 3, 4};
+mytype ms2[] = {1, 2, 2, 4};
+mytype ms3[] = {2, 2, 4, 4};
+mytype ms4[] = {2, 2, 3, 4};
+vm s(ms1, ms1 + 2);
+vm s1(ms1, ms1 + 4);
+vm s2(ms2, ms2 + 4);
+vm s3(ms3, ms3 + 4);
+vm s4(ms4, ms4 + 4);
+vm s5(ms4, ms4 + 2);
+
+BOOST_AUTO_TEST_CASE(operator_test)
+{
+	tmm t;
+	vm s;
+	s.push_back(mytype(1));
+	s.push_back(mytype(2));
+	s.push_back(mytype(3));
+	vm s2;
+	s2.push_back(mytype(2));
+	t[s] = 1;
+	BOOST_CHECK(t[s] == 1);
+	t[s] = 5;
+	BOOST_CHECK(t[s] == mytype(5));
+	t[s2] = t[s];
+	BOOST_CHECK(t[s] == t[s2]);
+	BOOST_CHECK(*t.begin() == mytype(5));
+	BOOST_CHECK(t.size() == 2);
+	BOOST_CHECK(t.count_node() == 4);
+}
+
+BOOST_AUTO_TEST_CASE(insert_and_find_test)
+{
+	tmm t;
+	t[s] = 1;
+	BOOST_CHECK(*t.find(s) == 1);
+	BOOST_CHECK(t.find(s) != t.end());
+	BOOST_CHECK(t.find(s2) == t.end());
+	BOOST_CHECK(t.find(s2) == t.end());
+	t[s] = 2;
+	BOOST_CHECK(*t.find(s) == 2);
+	t[s2] = t[s];
+	BOOST_CHECK(t.find(s2) != t.end());
+	BOOST_CHECK(t.find(s1) == t.find(s3));
+	BOOST_CHECK(t.find(s) != t.find(s2));
+	BOOST_CHECK((t.insert(s1, 3).second == true));
+	BOOST_CHECK((t.insert(s3, 10).second == true));
+	BOOST_CHECK((t.insert(s2, 10)).second == false);
+	BOOST_CHECK((t.insert(s, 10)).second == false);
+}
+
+BOOST_AUTO_TEST_CASE(copy_test)
+{
+	tmm t, t2;
+	t[s] = 1; t[s1] = 2; t[s2] = 3; t[s3] = 4;
+	t2 = t;
+	size_t node_cnt = 4 + 2 + 4;
+	BOOST_CHECK(t2.size() == 4);
+	BOOST_CHECK(t2.count_node() == node_cnt);
+	BOOST_CHECK(t2[s] == 1);
+	BOOST_CHECK(t2[s1] == 2);
+	BOOST_CHECK(t2[s2] == 3);
+	BOOST_CHECK(t2[s3] == 4);
+	tmm t3(t2);
+	BOOST_CHECK(t3.size() == 4);
+	BOOST_CHECK(t3.count_node() == node_cnt);
+	BOOST_CHECK(*t3.find(s) == 1);
+	BOOST_CHECK(*t3.find(s1) == 2);
+	BOOST_CHECK(*t3.find(s2) == 3);
+	BOOST_CHECK(*t3.find(s3) == 4);
+	BOOST_CHECK(t3.find(ms1, ms1 + 3) == t3.end());
+	BOOST_CHECK(t3.find(ms1, ms1 + 4) != t3.end());
+}
+
+BOOST_AUTO_TEST_CASE(iterator_operator_plus)
+{
+	tmm t;
+	BOOST_CHECK(t.empty() == true);
+	BOOST_CHECK(t.size() == 0);
+	BOOST_CHECK(t.begin() == t.end());
+	t[s] = 1;
+	t[s1] = 2;
+	t[s2] = 3;
+	BOOST_CHECK(t.begin() != t.end());
+	tmm::iterator ti;
+	ti = t.begin();
+	BOOST_CHECK(*ti == 1);
+	++ti;
+	BOOST_CHECK(*ti == 3);
+	BOOST_CHECK(t[s2] == 3);
+	++ti;
+	BOOST_CHECK(*ti == 2);
+	++ti;
+	BOOST_CHECK(ti == t.end());
+	// test ++end()
+	++ti;
+	BOOST_CHECK(ti == t.end());
+}
+
+BOOST_AUTO_TEST_CASE(iterator_operator_minus)
+{
+	tmm t;
+	BOOST_CHECK(t.empty() == true);
+	BOOST_CHECK(t.size() == 0);
+	BOOST_CHECK(t.begin() == t.end());
+	t[s] = 1;
+	t[s1] = 2;
+	t[s2] = 3;
+	BOOST_CHECK(t.begin() != t.end());
+	tmm::iterator ti;
+	tmm::const_iterator cti(t.end());
+	ti = t.begin();
+	BOOST_CHECK(*ti == 1);
+	*ti = 10;
+	BOOST_CHECK(*ti == 10);
+	cti = t.begin();
+	BOOST_CHECK(*cti == 10);
+	--ti;
+	BOOST_CHECK(ti == t.end());
+	BOOST_CHECK(t[s2] == 3);
+	--ti;
+	BOOST_CHECK(*ti == 2);
+	t[s3] = 4;
+	++ti;
+	BOOST_CHECK(*ti == 4);
+	++ti;
+	BOOST_CHECK(ti == t.end());
+	++ti;
+	BOOST_CHECK(ti == t.end());
+	--ti;
+	BOOST_CHECK(*ti == 4);
+	int node_cnt = 4 + 2 + 4 + 2;
+	t[s4] = 5;
+	t[s5] = 6;
+	BOOST_CHECK(t.count_node() == node_cnt);
+	BOOST_CHECK(t.size() == 6);
+}
+
+BOOST_AUTO_TEST_CASE(clear)
+{
+	tmm t;
+	t[s] = t[s1] = t[s2] = t[s3] = 10;
+	int node_cnt = t.count_node();
+	BOOST_CHECK(t.size() == 4);
+	BOOST_CHECK(t.count_node() == node_cnt);
+	t.clear();
+	BOOST_CHECK(t.size() == 0);
+	BOOST_CHECK(t.count_node() == 0);
+	BOOST_CHECK(t[s] == 0);
+	BOOST_CHECK(t.size() == 1);
+	BOOST_CHECK(t.count_node() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(erase_iterator)
+{
+	tmm t;
+	t[s] = 1;
+	t[s1] = 2;
+	t[s2] = 3;
+	t[s3] = 4;
+	int node_cnt = t.count_node();
+	BOOST_CHECK(t.size() == 4);
+	BOOST_CHECK(t.count_node() == node_cnt);
+	tmm::iterator ti;
+	ti = t.begin();
+	t.erase(t.begin());
+	ti = t.begin();
+	BOOST_CHECK(t.size() == 3);
+	//std::cout << t.count_node() << std::endl;
+	BOOST_CHECK(t.count_node() == node_cnt);
+	BOOST_CHECK(*ti == 3);
+	t.erase(ti);
+	BOOST_CHECK(t.size() == 2);
+	ti = t.begin();
+	BOOST_CHECK(*ti == 2);
+	BOOST_CHECK(t.count_node() == 8);
+	BOOST_CHECK(t[s] == 0);
+	ti = t.begin();
+	BOOST_CHECK(t.size() == 3);
+	BOOST_CHECK(*ti == 0);
+	BOOST_CHECK(t.count_node() == 8);
+}
+
+BOOST_AUTO_TEST_CASE(erase_key)
+{
+	tmm t;
+	t[s] = 1;
+	t[s1] = 2;
+	t[s2] = 3;
+	t[s3] = 4;
+	int node_cnt = t.count_node();
+	BOOST_CHECK(t.size() == 4);
+	BOOST_CHECK(t.count_node() == node_cnt);
+	tmm::iterator ti;
+	ti = t.begin();
+	t.erase(ms1, ms1 + 1);
+	ti = t.begin();
+	// erasion failure
+	BOOST_CHECK(t.size() == 4);
+	BOOST_CHECK(*ti == 1);
+	t.erase(ms1, ms1 + 2);
+	ti = t.begin();
+	BOOST_CHECK(t.size() == 3);
+	BOOST_CHECK(*ti == 3);
+	t.erase(s1);
+	BOOST_CHECK(t.size() == 2);
+	ti = t.begin();
+	BOOST_CHECK(*ti == 3);
+	BOOST_CHECK(t[s] == 0);
+	ti = t.begin();
+	BOOST_CHECK(t.size() == 3);
+	BOOST_CHECK(*ti == 0);
+	ti = t.begin();
+	BOOST_CHECK(t.size() == 3);
+	BOOST_CHECK(*ti == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
